257007fb859172e8c3032522f72c0ffc951528d1
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-27 12:42:47 +0900

    첫번째 mutation 만들기
    mutation 사용해서
    movie 추가하기

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,57 @@
-export const people = [
+let movies = [
     {
-        id: "0",
-        name: "Crystal",
-        age: 23,
-        gender: "female"
+        id: 0,
+        name: "7번방의 선물",
+        score: 8
     },
     {
-        id: "1",
-        name: "Jamie",
-        age: 16,
-        gender: "female"
+        id: 1,
+        name: "달마야 간다",
+        score: 4
     },
     {
-        id: "2",
-        name: "Sola",
-        age: 28,
-        gender: "female"
+        id: 2,
+        name: "장화홍련",
+        score: 3
     },
     {
-        id: "3",
-        name: "Jenny",
-        age: 22,
-        gender: "female"
+        id: 3,
+        name: "기생충",
+        score: 10
     },
     {
-        id: "4",
-        name: "Kai",
-        age: 30,
-        gender: "male"
+        id: 4,
+        name: "미나리",
+        score: 10
     },
     {
-        id: "5",
-        name: "hyuk",
-        age: 26,
-        gender: "male"
-    },
-    {
-        id: "6",
-        name: "finn",
-        age: 24,
-        gender: "male"
+        id: 5,
+        name: "미션임파서블",
+        score: 9
     }
 ];
 
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === String(id));
-    return filteredPeople[0];
+    const filteredMovies = movies.filter(movie => movie.id === String(id));
+    return filteredMovies[0];
+};
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+    if (movies.length > cleanedMovies.length) {
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+};
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
 };
